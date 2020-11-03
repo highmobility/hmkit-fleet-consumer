dbe7bf5d3a751718a5f60cf618bb376899ca879f
tonis <tonisives@gmail.com>
2020-11-03 09:42:38 +0700

    add test clearance request

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -1,6 +1,26 @@
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+import network.ClearVehicleResponse;
+import network.WebService;
+
 class WebServer {
-    public static void main(String[] args) {
-        FleetSdk sdk = FleetSdk.INSTANCE;
-        sdk.init("apiKey");
+    HMKitFleet hmkit = HMKitFleet.getInstance("apiKey");
+
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        new WebServer().start();
+    }
+
+    void start() throws ExecutionException, InterruptedException {
+        Executors.newCachedThreadPool().submit(() -> {
+            System.out.println("Start " + System.currentTimeMillis());
+            CompletableFuture requestClearance = hmkit.requestClearance("vin1");
+
+            ClearVehicleResponse response = (ClearVehicleResponse) requestClearance.get();
+
+            System.out.println("end " + System.currentTimeMillis());
+            return null;
+        });
     }
 }
